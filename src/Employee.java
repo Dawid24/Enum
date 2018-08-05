d0a7e02bb8d1e560d454e88160b1afba6bb56472
Dawid24 <paciorekdawidonly@gmail.com>
2018-08-05 23:11:25 +0200

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    Sex sex;
+    Salary salary;
+
+    public enum Salary {
+        one,
+        two,
+        three
+    }
+
+}
---
+++ src/car/evidence/v01/Brand.java
@@ -0,0 +1,13 @@
+package car.evidence.v01;
+
+public enum Brand {
+    Mercedes,
+    BMW,
+    Honda,
+    Lexus,
+    Toyota,
+    Skoda
+
+
+
+}
